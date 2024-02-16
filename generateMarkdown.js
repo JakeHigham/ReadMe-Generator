3982e59a21549491959e4bced1e702a2b86cff2b
Jacob Higham <149442786+JakeHigham@users.noreply.github.com>
2024-02-15 21:47:56 -0600

    Added updated javascript
    Added updated index.js, generateMarkdown.js has been moved from utils folder to main, package.json added, gitignore added

---
+++ generateMarkdown.js
@@ -0,0 +1,36 @@
+const generateMarkdown = data => {
+  return `# ${data.title}
+  ![Github licence](http://img.shields.io/badge/license-${data.license}-blue.svg)
+
+  
+  ## Description 
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Credits](#credits)
+  * [Screenshots](#screenshots)
+  * [Contact Me](#contact)
+  
+  ## Installation 
+  ${data.install}
+
+  ## Usage 
+  ${data.usage}
+
+  ## License 
+  This project is currently licensed under ${data.license}
+
+  ## Credits 
+  ${data.credits}
+
+  ## Contact Me!
+  If you have any questions, or ideas about this projects, please contact me directly at ${data.email}. You can view more of my projects at https://github.com/${data.github}.
+`;
+}
+
+
+
+module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,13 +1,83 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a brief description of your project:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'GNU', 'None', 'Other'],
+        default: ["None"],
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Selection required');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the steps required to install your project?',
+       default: ['N/A']
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this app?',
+        default: ['N/A']
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Are there any credits you would like to include?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email you would like to use?',
+        default: 'N/A'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?',
+        default: 'N/A'
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md', data, (err) =>
+        err ? console.error(err) : console.log(`${fileName} generated successfully!`)
+    );
+}
 
-// Function call to initialize app
+// Function to initialize app
+async function init() {
+    try {
+        const answers = await inquirer.prompt(questions);
+        const readmeContent = generateMarkdown(answers);
+        writeToFile('README.md', readmeContent);
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+// Call the init function to start the application
 init();
